926bb284df7ceddd78acba93cbddaba3eb514aa7
André Lucas Macedo Valle Santos <andrelucas_m.santos@outlook.com>
2026-07-20 05:38:00 -0300

    Tópico - Abstração Java

---
+++ curso_programacao_JAVA/src/Programacao_Orientada_Objetos/application/HerancaPolimorfismo/application/ProgramClasseAbstrata.java
@@ -0,0 +1,39 @@
+package Programacao_Orientada_Objetos.application.HerancaPolimorfismo.application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Programacao_Orientada_Objetos.entities.HerancaPolimorfismo.entities.Account;
+import Programacao_Orientada_Objetos.entities.HerancaPolimorfismo.entities.AccountAbstrata;
+import Programacao_Orientada_Objetos.entities.HerancaPolimorfismo.entities.BusinessAccount;
+import Programacao_Orientada_Objetos.entities.HerancaPolimorfismo.entities.SavingsAccount;
+
+public class ProgramClasseAbstrata {
+	
+	public static void main(String[] args) {
+		
+		// Instanciação de uma classe abstrata, não é possível:
+		
+		/*Account acc1 = new Account(1001, "Alex", 1000.00);
+		Account acc2 = new SavingsAccount(1002, "Maria", 1000.00, 0.01);
+		Account acc3 = new BusinessAccount(1003, "Bob", 1000.00, 500.00);*/
+		
+		
+		List<AccountAbstrata> list = new ArrayList<>();
+		
+		list.add(new SavingsAccount(1001, "Alex", 500.00, 0.01));
+		list.add(new BusinessAccount(1002, "Maria", 1000.00, 400.00));
+		list.add(new SavingsAccount(1004, "Bob", 300.00, 0.01));
+		list.add(new BusinessAccount(1005, "Anna", 500.00, 500.00));
+		
+		double sum = 0.0;
+		
+		for (AccountAbstrata acc : list) {
+			sum += acc.getBalance();	
+		}
+		
+		System.out.printf("Total balance: %2.f%n", sum);
+		
+	}
+
+}
---
+++ curso_programacao_JAVA/src/Programacao_Orientada_Objetos/entities/HerancaPolimorfismo/entities/AccountAbstrata.java
@@ -0,0 +1,53 @@
+package Programacao_Orientada_Objetos.entities.HerancaPolimorfismo.entities;
+
+public abstract class AccountAbstrata {
+	
+	private Integer number;
+	private String holder;
+	protected Double balance; // Modificador de acesso - altera a visualização pela subclasse
+	
+	public AccountAbstrata() {
+	}
+
+	public AccountAbstrata(Integer number, String holder, Double balance) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void withdraw(double amount) { // Saque
+		balance -= amount + 5.0;
+	}
+	
+	public void deposit(double amount) {
+		balance += amount;
+	}
+
+	@Override
+	public String toString() {
+		return "Account [number=" + number + ", holder=" + holder + ", balance=" + balance + ", getNumber()="
+				+ getNumber() + ", getHolder()=" + getHolder() + ", getBalance()=" + getBalance() + ", getClass()="
+				+ getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
+	}
+
+}
